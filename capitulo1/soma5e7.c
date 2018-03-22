bfb9e2b3333daebdd5899ee9e876388b794a8aae
Francisco de Assis Boldt <fboldt@gmail.com>
2018-03-22 11:09:53 -0300

    Add files via upload

---
+++ capitulo1/soma5e7.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+int main() {
+  printf("5 + 7 = %d\n", 5+7);
+  return 0;
+}
---
+++ capitulo1/somaxy.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main() {
+  int x, y;
+  x = 5;
+  y = 7;
+  printf("%d + %d = %d\n", x, y, x+y);
+  return 0;
+}
